c037425a6f84fa02efc6c93f4eb791a1ecdc3d29
turbosuns7 <turbosuns7@gmail.com>
2022-11-03 16:47:16 +0300

    mix puzzle

--- app/src/main/java/com/example/myapplication/ui/sudoku/TagGameFragment.java
+++ app/src/main/java/com/example/myapplication/ui/sudoku/TagGameFragment.java
@@ -10,6 +10,8 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import android.os.Handler;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -23,6 +25,13 @@ import android.widget.Toast;
 import com.example.myapplication.MyButton;
 import com.example.myapplication.R;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Timer;
+import java.util.TimerTask;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
 public class TagGameFragment extends Fragment {
 
     private TagGameViewModel mViewModel;
@@ -32,6 +41,10 @@ public class TagGameFragment extends Fragment {
         return new TagGameFragment();
     }
     private MyButton EmptyButton = null;
+    private MyButton LastMove = null;
+    private final Lock lock = new ReentrantLock();
+
+    Timer timer = new Timer();
 
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
@@ -43,10 +56,64 @@ public class TagGameFragment extends Fragment {
             for (int j = 0; j < columns; j++)
                 createElement(view, i, j);
 
+        Button b = (Button)view.findViewById(R.id.startGameButton);
+        b.setText("start");
+        b.setOnClickListener(new View.OnClickListener() {
+
+            Handler h = new Handler();
+
+            @Override
+            public void onClick(View v) {
+                Button b = (Button) v;
+                if (b.getText() == "start") {
+                    timer.schedule(new TimerTask() {
+                        @Override
+                        public void run() {
+                            //use a handler to run a toast that shows the current timestamp
+                            lock.lock();
+                            h.post(new Runnable() {
+
+                                public void run() {
+                                    MyButton b = (MyButton) getNextRandomButton();
+
+                                    lock.lock();
+                                    if (b != null) {
+                                        b.callOnClick();
+                                        Log.d("Move", b.row + " : " + b.column + " , Empty: " + EmptyButton.row + " : " + EmptyButton.column);
+                                    }
+                                    else {
+                                        Log.d("Move","Button is null");
+                                    }
+                                    lock.unlock();
+                                }
+
+                            });
+                            lock.unlock();
+                        }
+                    }, 100, 500);
+                    b.setText("stop");
+                }
+                else
+                {
+                    timer.cancel();
+                    b.setText("start");
+                }
+            }
+        });
+
+
+
        return view;
     }
 
 
+    @Override
+    public void onDestroyView() {
+
+        super.onDestroyView();
+
+        //timer.cancel();
+    }
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
@@ -60,25 +127,22 @@ public class TagGameFragment extends Fragment {
 
         grid[i][j] = myButton;
         if (i == 0 && j == 0) {
-            //myButton.setVisibility(View.GONE);
             myButton.getBackground().setAlpha(0);
             myButton.setTextColor(Color.TRANSPARENT);
             EmptyButton = myButton;
             grid[i][j] = null;
         }
 
-        final Animation animAlpha = AnimationUtils.loadAnimation(getActivity(), R.anim.alfa);
         myButton.setOnClickListener(new View.OnClickListener() {
 
         @Override
         public void onClick(View view) {
-            Toast.makeText(getContext(), "Empty " + EmptyButton.getX() + ":" + EmptyButton.getY(),Toast.LENGTH_SHORT).show();
+            //Toast.makeText(getContext(), "Empty " + EmptyButton.getX() + ":" + EmptyButton.getY(),Toast.LENGTH_SHORT).show();
             MyButton button = (MyButton) view;
 
             if (Math.abs(button.row - EmptyButton.row) == 1 && button.column == EmptyButton.column
                     || Math.abs(button.column - EmptyButton.column) == 1 && button.row == EmptyButton.row)
             {
-                ViewPropertyAnimator animator = button.animate().x(EmptyButton.getX()).y(EmptyButton.getY()).setDuration(400);
 
                 int tmpRow = button.row;
                 button.row = EmptyButton.row;
@@ -91,6 +155,11 @@ public class TagGameFragment extends Fragment {
                 ViewGroup.LayoutParams tmp = button.getLayoutParams();
                 button.setLayoutParams(EmptyButton.getLayoutParams());
                 EmptyButton.setLayoutParams(tmp);
+
+
+                LastMove = button;
+
+                //button.animate().x(EmptyButton.getX()).y(EmptyButton.getY()).setDuration(200);
 
             }
 
@@ -110,39 +179,29 @@ public class TagGameFragment extends Fragment {
 
     }
 
-    public void onToDoItemClick(View view) {
-        //ToDo item = ToDo.findById(ToDo.class, view.getId());
-        Toast.makeText(getContext(), view.getId() + "",Toast.LENGTH_SHORT).show();
+    private synchronized Button getNextRandomButton()
+    {
+
+        ArrayList<Button> possibleMove = new ArrayList<Button>();
+        if (EmptyButton.row > 0)
+            possibleMove.add(grid[EmptyButton.row - 1][EmptyButton.column]);
+        if (EmptyButton.row < rows - 2)
+            possibleMove.add(grid[EmptyButton.row + 1][EmptyButton.column]);
+        if (EmptyButton.column > 0)
+            possibleMove.add(grid[EmptyButton.row][EmptyButton.column - 1]);
+        if (EmptyButton.column < columns - 2)
+            possibleMove.add(grid[EmptyButton.row][EmptyButton.column + 1]);
+
+        //possibleMove.remove(LastMove);
+
+        int index = rnd(possibleMove.size() - 1);
+
+        return possibleMove.get(index);
     }
 
-
-    private void moveTo(MyButton button, int toRow, int toColumn)
+    public static int rnd(int max)
     {
-        GridLayout.LayoutParams oldLp = new GridLayout.LayoutParams(
-            GridLayout.spec(button.row, GridLayout.CENTER),
-            GridLayout.spec(button.column, GridLayout.CENTER));
-
-        GridLayout.LayoutParams newLp = new GridLayout.LayoutParams(
-                GridLayout.spec(toRow, GridLayout.CENTER),
-                GridLayout.spec(toColumn, GridLayout.CENTER));
-
-
-        grid[toRow][toColumn] = button;
-        grid[button.row][button.column] = null;
-
-
-
-        Button b = new Button(getContext());
-        b.setLayoutParams(newLp);
-        int newTop = b.getTop();
-        int newLeft = b.getLeft();
-
-        //b.setLayoutParams(newLp);
-        //button.animate().x(newLeft).y(newTop).setDuration(400);
-        //button.setLayoutParams(newLp);
-
-        //button.animate().x(newLeft).y(newTop).setDuration(400);
-        //button.setLayoutParams(newLp);
+        return (int) (Math.random() * ++max);
     }
 
 }
