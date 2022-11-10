de9ca8ba1a1ec5e56a84e318d2cbb8cd4083c7c4
turbosuns7 <turbosuns7@gmail.com>
2022-11-10 13:12:49 +0300

    puzzle records

---
+++ app/src/main/java/com/example/myapplication/ui/PuzzleRecordsFragment.java
@@ -0,0 +1,38 @@
+package com.example.myapplication.ui;
+
+import androidx.lifecycle.ViewModelProvider;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.myapplication.R;
+
+public class PuzzleRecordsFragment extends Fragment {
+
+    private PuzzleRecordsViewModel mViewModel;
+
+    public static PuzzleRecordsFragment newInstance() {
+        return new PuzzleRecordsFragment();
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_puzzle_records, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        mViewModel = new ViewModelProvider(this).get(PuzzleRecordsViewModel.class);
+        // TODO: Use the ViewModel
+    }
+
+}
---
+++ app/src/main/java/com/example/myapplication/ui/PuzzleRecordsViewModel.java
@@ -0,0 +1,7 @@
+package com.example.myapplication.ui;
+
+import androidx.lifecycle.ViewModel;
+
+public class PuzzleRecordsViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
